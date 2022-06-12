fd831b234557c7cd556dd55b70a515dad4a41b27
sanketk16 <khadke.s@northeastern.edu>
2022-06-12 12:48:41 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,9 @@
       new Typed('.typed', {
         strings: typed_strings,
         loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000
+        typeSpeed: 70,
+        backSpeed: 45,
+        backDelay: 1700
       });
     }
   
